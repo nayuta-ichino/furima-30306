58b48915c8d78545f98fdeb2c066eba9fb6c99d7
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2020-12-21 20:50:40 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,25 +7,23 @@ class User < ApplicationRecord
   validates :nickname, :birthday, presence: true
 
   # パスワードのバリデーション
-  VALID_PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
-  validates :password, 
-             presence: true,
-             length: {minimum: 6},
-             format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/.freeze
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            format: { with: VALID_PASSWORD_REGEX }
 
   # 苗字名前のバリデーション
-  VALID_NAME_REGEX = /\A([ぁ-んァ-ン一-龥]|ー)+$\Z/
+  VALID_NAME_REGEX = /\A([ぁ-んァ-ン一-龥]|ー)+$\Z/.freeze
   validates :first_name, :last_name,
-             presence: true,
-             format: { with: VALID_NAME_REGEX }
+            presence: true,
+            format: { with: VALID_NAME_REGEX }
 
   # 苗字名前カナのバリデーション
-  VALID_NAMEKANA_REGEX = /\A([ァ-ン]|ー)+$\Z/
+  VALID_NAMEKANA_REGEX = /\A([ァ-ン]|ー)+$\Z/.freeze
   validates :first_name_kana, :last_name_kana,
-             presence: true,
-             format: { with: VALID_NAMEKANA_REGEX }
-
-
+            presence: true,
+            format: { with: VALID_NAMEKANA_REGEX }
 
   has_many :items
   has_many :buy_records
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,57 +5,56 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-
   describe 'ユーザー新規登録' do
     it 'nicknameが空だと登録ができない' do
-      @user.nickname = ""
+      @user.nickname = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'メールアドレスが空だと登録ができない' do
-      @user.email = ""
+      @user.email = ''
       @user.valid?
       # binding.pry
       expect(@user.errors.full_messages).to include "Email can't be blank"
     end
     it 'パスワードが空だと登録ができない' do
-      @user.password = ""
+      @user.password = ''
       @user.valid?
       # binding.pry
-      expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
     end
     it '確認用パスワードが一致しないと登録ができない' do
-      @user.password_confirmation = ""
+      @user.password_confirmation = ''
       @user.valid?
       # binding.pry
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
     it '苗字が空だと登録ができない' do
-      @user.last_name = ""
+      @user.last_name = ''
       @user.valid?
       # binding.pry
-      expect(@user.errors.full_messages).to include "Last name can't be blank", "Last name is invalid"
+      expect(@user.errors.full_messages).to include "Last name can't be blank", 'Last name is invalid'
     end
     it '名前が空だと登録ができない' do
-      @user.first_name = ""
+      @user.first_name = ''
       @user.valid?
       # binding.pry
-      expect(@user.errors.full_messages).to include  "First name can't be blank", "First name is invalid"
+      expect(@user.errors.full_messages).to include "First name can't be blank", 'First name is invalid'
     end
     it '苗字カナが空だと登録ができない' do
-      @user.last_name_kana = ""
+      @user.last_name_kana = ''
       @user.valid?
       # binding.pry
-      expect(@user.errors.full_messages).to include "Last name kana is invalid"
+      expect(@user.errors.full_messages).to include 'Last name kana is invalid'
     end
     it '名前カナが空だと登録ができない' do
-      @user.first_name_kana = ""
+      @user.first_name_kana = ''
       @user.valid?
       # binding.pry
-      expect(@user.errors.full_messages).to include "First name kana can't be blank", "First name kana is invalid"
+      expect(@user.errors.full_messages).to include "First name kana can't be blank", 'First name kana is invalid'
     end
     it '誕生日が空だと登録ができない' do
-      @user.birthday = ""
+      @user.birthday = ''
       @user.valid?
       # binding.pry
       expect(@user.errors.full_messages).to include "Birthday can't be blank"
